468c384e42af8606fabd57b7e442b7f4099471a7
Mathias Benites <i202406644@cibertec.edu.pe>
2026-02-28 15:58:45 -0500

    tamareeeee

--- src/main/java/cibertec/pe/feignClient/CursoFeignClient.java
+++ src/main/java/cibertec/pe/feignClient/CursoFeignClient.java
@@ -3,7 +3,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "SOAP-Curso", url = "{feign.curso.url}")
+@FeignClient(name = "SOAP-Curso", url = "${feign.curso.url}")
 public interface CursoFeignClient {
     
     @GetMapping("/api/curso/{id}")
